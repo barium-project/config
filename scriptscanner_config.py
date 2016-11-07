af4108a2e21a4505481f379df27d3fff05fcb2f7
theoriginaljuice <physics.justin.christensen@gmail.com>
2016-11-07 12:52:53 -0800

    Added simpler loading exp using the cam

--- scriptscanner_config.py
+++ scriptscanner_config.py
@@ -8,7 +8,7 @@ class config(object):
 
     # list in the format (import_path, class_name)
     scripts = [(exps + 'linescan_camera.linescan_camera', 'linescan_camera'), (exps + 'filament_loading.filament_loading', 'filament_loading'),(exps + 'frequency_scan.frequency_scan', 'frequency_scan'), \
-               (exps + 'loading_curve.loading_curve', 'loading_curve')]
+               (exps + 'loading_curve.loading_curve', 'loading_curve'),(exps + 'loading_curve_cam.loading_curve_cam','loading_curve_cam')]
 
     #allowed_concurrent = {}
     #allowed_concurrent['lasermonitor'] = ['lasermonitor']
